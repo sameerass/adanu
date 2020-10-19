3d32ca1db9b26ef57aee5067886829e1bf4d1748
sameerass <72432326+sameerass@users.noreply.github.com>
2020-10-19 14:21:21 -0700

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
 import { PropsWithChildren } from 'react';
-import { PREFIX } from '../constants';
 
 export interface IProps {
   title: string;
@@ -12,8 +11,7 @@ const Layout = (props: PropsWithChildren<IProps>) => {
         <Head>
             <title>Adanu | {props.title} |inspiring communities together</title>
             <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'/>
-            <base href={`/${PREFIX}`}/>
-            <link rel='shortcut icon' href={`${PREFIX}/favicon.ico`} />
+            <link rel='shortcut icon' href={`/favicon.ico`} />
             <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
             <script src='http://malsup.github.com/jquery.cycle2.js'></script>
         </Head>

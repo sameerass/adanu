91dbb32f161bd190d3920579bf0869d91659204d
Sameera Subair <sameeras@themosaiccompany.com>
2020-10-20 08:49:52 -0700

    Added base url

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,11 +7,13 @@ import { PREFIX } from '../constants';
 export interface IProps {
   title: string;
 }
+const baseUrl = process.env.PUBLIC_URL + (process.env.NEXT_PUBLIC_BASE_PATH || '');
 const Layout = (props: PropsWithChildren<IProps>) => {
   return (<div>
         <Head>
             <title>Adanu | {props.title} |inspiring communities together</title>
             <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no'/>
+            <base href={baseUrl}/>
             <link rel='shortcut icon' href={`/${PREFIX}/favicon.ico`} />
             <script src='http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js'></script>
             <script src='http://malsup.github.com/jquery.cycle2.js'></script>
